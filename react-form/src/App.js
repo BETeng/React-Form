2b3c4f90ef832b1a9ac2ad1b59c5929db53245b0
LG BeTeng <brian7.teng@gmail.com>
2020-01-15 15:09:24 -0600

    form aditon

--- react-form/src/App.js
+++ react-form/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        <Form/>
+      </div>
+    )
+  }
 }
+
+
+
+
 
 export default App;
---
+++ react-form/src/Form.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+
+    
+    render() {
+        return (
+            <form>
+                <div>
+                    <label>Username</label>
+                    <input type='text'></input>
+                </div>
+            </form>
+        )
+    }
+}
+
+
+
+
+
+export default Form;
